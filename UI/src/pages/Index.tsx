b97fbfceff118aaeece71f348996b48d2e114a79
BennyPerumalla <benny01r@gmail.com>
2026-08-25 23:25:32 +0530

    style(ui): configure dark theme typography, layout dimensions, and glassmorphism styling

--- UI/src/pages/Index.tsx
+++ UI/src/pages/Index.tsx
@@ -1,11 +1,7 @@
 import { AudioEqualizer } from "@/equalizer/AudioEqualizer";
 
 const Index = () => {
-  return (
-    <main className="app-canvas">
-      <AudioEqualizer />
-    </main>
-  );
+  return <AudioEqualizer />;
 };
 
 export default Index;
